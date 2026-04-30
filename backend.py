c431212cbd3743a748facd9cf629d70eafbc809a
Stefan Thaler <120387118+SjjThaler@users.noreply.github.com>
2026-04-30 23:52:44 +0200

    Change User-Agent to curl in backend.py

--- backend.py
+++ backend.py
@@ -28,8 +28,7 @@ queries = {
 }
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "User-Agent": "curl/8.5.0",
     "Accept-Language": "en-US,en;q=0.9",
 }
 
